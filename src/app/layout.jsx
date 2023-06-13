8bcd5b48e841b9aa933666d18259a1e693a21e77
Stephen Afari <demorganafari@yahoo.com>
2023-06-13 06:15:59 +0000

    added the Navbar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import './globals.css'
 import Providers from './Providers'
+import Navbar from '@/components/Navbar'
 
 
 export default function RootLayout({ children }) {
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
   {/* Header */}
       <Header/>
       {/* Navbar*/}
+      <Navbar/>
 
        {/* Searchbox */}
         {children}
